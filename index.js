958364121230db3c81a600998b7c738daad64e9f
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-05-04 23:27:41 -0700

    updated hasedpassword for put request

--- index.js
+++ index.js
@@ -263,12 +263,13 @@ app.put(
 			return res.status(422).json({ errors: errors.array() });
 		}
 
+		let hashedPassword = Users.hashPassword(req.body.Password);
 		Users.findOneAndUpdate(
 			{ Username: req.params.Username },
 			{
 				$set: {
 					Username: req.body.Username,
-					Password: req.body.Password,
+					Password: hashedPassword,
 					Email: req.body.Email,
 					Birthday: req.body.Birthday,
 				},
