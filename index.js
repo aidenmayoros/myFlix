acee266c398c9b8fea70cd2c215bb7dbcb0071f8
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-05-20 10:29:28 -0700

    removed authentication from /movies

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const passport = require('passport');
 require('./passport');
 
 // Get all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
 	Movies.find()
 		.then((movies) => {
 			res.status(200).json(movies);
