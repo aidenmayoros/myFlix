9f56c29b7cf4a3086dd06362eafb6ead3607afb0
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-06-25 10:09:03 -0700

    removed unneeded CORS url

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ let allowedOrigins = [
 	'http://localhost:8080',
 	'http://testsite.com',
 	'http://localhost:1234',
-	'http://localhost:1234/',
 	'https://aidens-myflix-api.herokuapp.com',
 	'https://git.heroku.com/aidens-myflix-api.git',
 ];
